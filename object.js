75228f740cdf2ff39d37ff84a2510c104a806a7c
theapekshya <139621450+theapekshya@users.noreply.github.com>
2023-11-04 21:37:01 +0545

    method_in_object
    in js

---
+++ object.js
@@ -0,0 +1,20 @@
+// object in JS
+
+// let person = {
+//     name: "Apekshya",
+//     age: 22
+// };
+// console.log(person.name);
+
+
+
+// method in object in JS
+let person = {
+    name: "Apekshya",
+    age: 22,
+    sayhello: function() {
+        document.write("hello")
+    }
+};
+
+person.sayhello();
